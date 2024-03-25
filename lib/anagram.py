b97877a0adc4642d856c1c8be4ff8f4b3923a36a
Joe <jakatsa5@gmail.com>
2024-03-25 03:45:19 +0300

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+# class
+# initalize , take in param (word)
+# def match to check if the word marches 
+
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, array):
+        # for loop
+        # sort bothr the word and the word in array
+        #  if the same in a list
+            
+        anagram_list = [arr_word for arr_word in array if sorted(self.word)== sorted(arr_word)]
+        return anagram_list
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
